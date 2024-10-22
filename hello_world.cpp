6e214f1f173f8aa0e22964519a700cb4891e81da
SDaKko <danilkosenko172004.ru>
2024-10-23 00:47:34 +0300

    Добавлены комментарии к коду

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::string name;
     std::cout << "Input your name: ";
+    //¬водим им€
     std::cin >> name;
     std::cout << "Hello, World from " << name << std::endl;
     return 0;
