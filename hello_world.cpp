fefdeedfddb0c586062f4d1fa55d059e8d579f01
SDaKko <danilkosenko172004.ru>
2024-10-23 21:57:20 +0300

    Изменение файла для создания конфликта

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@
 int main() {
     std::string name;
     std::cout << "Input your name: ";
-    //input username
+    //input. username.
     std::cin >> name;
-    //print "hello, world"
+    //print. "hello, world"
     std::cout << "Hello, World from " << name << std::endl;
     return 0;
 }
