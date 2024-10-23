a8ca5d48471a021fd9fab63a941c3978ca2fb9ca
SDaKko <danilkosenko172004.ru>
2024-10-24 00:22:02 +0300

    Изменен стиль кода с помощью clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-int main() {
-    std::string name;
-    std::cout << "Input your name: ";
-    //input. username.
-    std::cin >> name;
-    //print. "hello, world"
-    std::cout << "Hello, World from " << name << std::endl;
-    return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Input your name: ";
+  // input username
+  std::cin >> name;
+  // print "hello, world"
+  std::cout << "Hello, World from " << name << std::endl;
+  return 0;
 }
